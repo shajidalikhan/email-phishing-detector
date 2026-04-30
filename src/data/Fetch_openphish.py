7a4737398d57705a5f230b09e736f741032673c3
henzlo <ekkaharshit556@gmail.com>
2026-05-01 05:02:09 +0530

    commet

---
+++ src/data/Fetch_openphish.py
@@ -0,0 +1,9 @@
+import requests
+import pandas as pd
+
+print("Fetching OpenPhish URLs...")
+response = requests.get("https://openphish.com/feed.txt")
+openphish_urls = response.text.strip().split("\n")
+
+print(f"OpenPhish URLs fetched: {len(openphish_urls)}")
+print(openphish_urls[:5])
